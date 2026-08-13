c8065ae5f9f9dcc52e6c36fac588800cd6c32aaf
justkinou <justkinou@proton.me>
2026-08-13 22:33:18 +0200

    feat: utilize sqlx compile time checking macros

--- src/database/tracks.rs
+++ src/database/tracks.rs
@@ -1,5 +1,3 @@
-use sqlx::{Row, prelude::FromRow};
-
 use crate::database::MusicDb;
 
 pub trait TracksDb {
@@ -16,8 +14,8 @@ pub trait TracksDb {
 impl TracksDb for MusicDb {
     async fn get_full_track_info(&self, track_id: i64) -> Result<FullTrackInfo, sqlx::Error> {
         let mut tx = self.pool.begin().await?;
-        
-        let query = r"
+
+        let track_info = sqlx::query!(r"
             SELECT
                 T.id,
                 name,
@@ -25,26 +23,14 @@ impl TracksDb for MusicDb {
                 thumbnail_url,
                 duration_seconds,
                 play_count,
-                T.user_id,
-                COUNT(L.user_id) like_count
+                T.user_id
             FROM tracks T
-            LEFT JOIN likes L
-            ON T.id = L.track_id
-            WHERE T.id = $1
-            GROUP BY
-                T.id,
-                name,
-                description,
-                thumbnail_url,
-                duration_seconds,
-                play_count,
-                T.user_id;
-        ";
-        let track_info = sqlx::query(query)
-            .bind(track_id)
+            WHERE T.id = $1;",
+            track_id,
+        )
             .fetch_one(&mut *tx)
             .await?;
-
+        
         let query = r"
             SELECT C.id, C.name
             FROM categories C
@@ -60,58 +46,54 @@ impl TracksDb for MusicDb {
         tx.commit().await?;
 
         let res = FullTrackInfo {
-            id: track_info.try_get("id")?,
-            name: track_info.try_get("name")?,
-            description: track_info.try_get("description")?,
-            thumbnail_url: track_info.try_get("thumbnail_url")?,
-            duration_seconds: track_info.try_get("duration_seconds")?,
-            play_count: track_info.try_get("play_count")?,
-            like_count: track_info.try_get("like_count")?,
-            user_id: track_info.try_get("user_id")?,
+            id: track_info.id,
+            name: track_info.name,
+            description: track_info.description,
+            thumbnail_url: track_info.thumbnail_url,
+            duration_seconds: track_info.duration_seconds,
+            play_count: track_info.play_count,
+            user_id: track_info.user_id,
             categories: track_categories,
         };
         Ok(res)
     }
 
     async fn get_short_track_info(&self, track_id: i64) -> Result<ShortTrackInfo, sqlx::Error> {
-        let query = r"
+        let res = sqlx::query_as!(ShortTrackInfo, r"
             SELECT
                 id, name, thumbnail_url, duration_seconds, play_count, user_id
             FROM tracks
-            WHERE id = $1;
-        ";
-        let res = sqlx::query_as::<_, ShortTrackInfo>(query)
-            .bind(track_id)
+            WHERE id = $1;",
+            track_id
+        )
             .fetch_one(&self.pool)
             .await?;
         Ok(res)
     }
 
     async fn save_track_info(&self, track_info: &UploadTrackInfo) -> Result<i64, sqlx::Error> {
-        let query = r"
+        let id: i64 = sqlx::query_scalar!(r"
             INSERT INTO tracks (
                 name, description, user_id
             ) VALUES ($1, $2, $3)
-            RETURNING id;
-        ";
-        let id: i64 = sqlx::query_scalar(query)
-            .bind(&track_info.name)
-            .bind(&track_info.description)
-            .bind(track_info.user_id)
+            RETURNING id;",
+            &track_info.name,
+            track_info.description,
+            track_info.user_id,
+        )
             .fetch_one(&self.pool)
             .await?;
         Ok(id)
     }
 
     async fn update_track_info(&self, track_id: i64, track_info: &UpdateTrackInfo) -> Result<(), sqlx::Error> {
-        let query = r"
+        let res = sqlx::query!(r"
             UPDATE tracks SET name = $1, description = $2
-            WHERE id = $3;
-        ";
-        let res = sqlx::query(query)
-            .bind(&track_info.name)
-            .bind(&track_info.description)
-            .bind(track_id)
+            WHERE id = $3;",
+            &track_info.name,
+            track_info.description,
+            track_id,
+        )
             .execute(&self.pool)
             .await?;
         if res.rows_affected() == 0 {
@@ -121,13 +103,12 @@ impl TracksDb for MusicDb {
     }
 
     async fn update_track_thumbnail(&self, track_id: i64, thumbnail_url: &str) -> Result<(), sqlx::Error> {
-        let query = r"
+        let res = sqlx::query!(r"
             UPDATE tracks SET thumbnail_url = $1
-            WHERE id = $2;
-        ";
-        let res = sqlx::query(query)
-            .bind(thumbnail_url)
-            .bind(track_id)
+            WHERE id = $2;",
+            thumbnail_url,
+            track_id,
+        )
             .execute(&self.pool)
             .await?;
         if res.rows_affected() == 0 {
@@ -137,9 +118,7 @@ impl TracksDb for MusicDb {
     }
 
     async fn remove_track_info(&self, track_id: i64) -> Result<(), sqlx::Error> {
-        let query = "DELETE FROM tracks WHERE id = $1;";
-        let res = sqlx::query(query)
-            .bind(track_id)
+        let res = sqlx::query!("DELETE FROM tracks WHERE id = $1;", track_id)
             .execute(&self.pool)
             .await?;
         if res.rows_affected() == 0 {
@@ -149,30 +128,31 @@ impl TracksDb for MusicDb {
     }
 
     async fn link_track_audio(&self, track_id: i64, link_info: &LinkAudioInfo) -> Result<(), sqlx::Error> {
-        let query = r"
+        sqlx::query!(r"
             UPDATE tracks SET audio_uri = $1, duration_seconds = $2
-            WHERE id = $3
-        ";
-        sqlx::query(query)
-            .bind(&link_info.audio_uri)
-            .bind(link_info.duration_seconds)
-            .bind(track_id)
+            WHERE id = $3;",
+            &link_info.audio_uri,
+            link_info.duration_seconds,
+            track_id,
+        )
             .execute(&self.pool)
             .await?;
         Ok(())
     }
 
     async fn register_play(&self, track_id: i64) -> Result<String, sqlx::Error> {
-        let query = r"
+        let audio_uri = sqlx::query_scalar!(r"
             UPDATE tracks SET play_count = play_count + 1
             WHERE id = $1
-            RETURNING audio_uri;
-        ";
-        let audio_uri: String = sqlx::query_scalar(query)
-            .bind(track_id)
+            RETURNING audio_uri;",
+            track_id,
+        )
             .fetch_one(&self.pool)
             .await?;
-        Ok(audio_uri)
+        match audio_uri {
+            Some(uri) => Ok(uri),
+            None => Err(sqlx::Error::RowNotFound),
+        }
     }
 }
 
@@ -184,12 +164,11 @@ pub struct FullTrackInfo {
     pub thumbnail_url: Option<String>,
     pub duration_seconds: Option<i64>,
     pub play_count: i64,
-    pub like_count: i64,
     pub user_id: i64,
     pub categories: Vec<CategoryInfo>,
 }
 
-#[derive(FromRow, Debug)]
+#[derive(sqlx::FromRow, Debug)]
 pub struct ShortTrackInfo {
     pub id: i64,
     pub name: String,
@@ -211,7 +190,7 @@ pub struct UpdateTrackInfo {
     pub category_ids: Vec<i64>,
 }
 
-#[derive(FromRow, Debug)]
+#[derive(sqlx::FromRow, Debug)]
 pub struct CategoryInfo {
     pub id: i64,
     pub name: String,
